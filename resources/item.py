9252fe597e103e963cc87a145ecf1ee4cd9f4791
irisida <20445648+irisida@users.noreply.github.com>
2020-08-02 23:15:47 +0100

    remove manually adding store

--- resources/item.py
+++ resources/item.py
@@ -6,7 +6,7 @@ from models.store import StoreModel
 
 class ItemList(Resource):
     def get(self):
-        return {"items": [item.json() for item in ItemModel.query.all()]}
+        return {"items": [item.json() for item in ItemModel.find_all()]}
 
 
 class Item(Resource):
@@ -35,8 +35,8 @@ class Item(Resource):
 
         data = Item.parser.parse_args()
         item = ItemModel(name, **data)
-        store = StoreModel(data["store_id"])
-        store.save_to_db()
+        # store = StoreModel(data["store_id"])
+        # store.save_to_db()
         item.save_to_db()
         return item.json(), 201
 
