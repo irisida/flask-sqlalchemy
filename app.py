ddc8cdc2383c23edbf33edc6c9c9e204ef8edbed
irisida <20445648+irisida@users.noreply.github.com>
2020-08-02 22:05:47 +0100

    added postgres config for heroku

--- app.py
+++ app.py
@@ -1,3 +1,5 @@
+import os
+
 from flask import Flask
 from flask_jwt_extended import JWTManager
 from flask_restful import Api
@@ -9,7 +11,9 @@ from resources.item import Item, ItemList
 from resources.store import Store, StoreList
 
 app = Flask(__name__)
-app.config["SQLALCHEMY_DATABASE_URI"] = "sqlite:///data.db"
+app.config["SQLALCHEMY_DATABASE_URI"] = os.environ.get(
+    "DATABASE_URL", "sqlite:///data.db"
+)
 app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
 app.config["JWT_SECRET_KEY"] = "flaskymcflaskface"
 api = Api(app)
